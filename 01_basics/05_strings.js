18e047f3c1dc4cacace4e18067c32b0a3f947e11
gomzzy21 <gomateshbastawad1997@gmail.com>
2024-09-19 04:42:53 +0000

    strings _done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name ="gomzyyy"
+const project =20
+// declared two varibles with their values
+
+console.log( "my name is " + name +" i have completed " + project + " projects");
+// this way we use before for concating two variables . but its bad practic and confusing one better we use template litterals
+
+//basically it called string interpollation
+console.log(`my name is ${name} and i have completed ${project} projects`);
+// its good practice and readable .by using this we ll directly using string methods in log itself .
+// like ${name.toUppercase} these kind methods we ll use    
+
+
+// and there are many strings are there in we can actually go through with MDN docs as well as in browser console also.  
+//example toUpperCase ,toLowerCase , subString ,IndexOf,String.Contains,split,join,lengthof
+
+let name2 = "gomzzz@21"
+
+console.log(name2.toUpperCase())
+
+console.log(name2.concat(" alfa "))
+
+// like this many of the methods ll use in js to handle the strings in js 
+
